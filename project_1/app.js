d781b4620972ebd554c2172994b7c8fb983e0417
Mach1r0 <segundacontafd@gmail.com>
2023-12-28 02:03:58 -0300

    update the javascript, and finish the footer

--- project_1/app.js
+++ project_1/app.js
@@ -1,5 +1,5 @@
-    const menu = documento.querySelector('#mobile-menu');
-    const menuLinks = documento.querySelector('.navvar__menu');
+    const menu = document.querySelector('#mobile-menu');
+    const menuLinks = document.querySelector('.navbar__menu');
 
     // Display Mobile Menu
     const mobileMenu = () => {
@@ -8,3 +8,35 @@
     }
 
     menu.addEventListener('click', mobileMenu);
+
+    // Smooth scrolling
+    const links = document.querySelectorAll(".navbar__menu a");
+
+    for (const link of links) {
+        link.addEventListener("click", clickHandler);
+    }
+
+    function clickHandler(e) {
+        e.preventDefault();
+        const href = this.getAttribute("href");
+        const offsetTop = document.querySelector(href).offsetTop;
+
+        scroll({
+            top: offsetTop,
+            behavior: "smooth"
+        });
+    }
+
+    //Show active menu when scrolling 
+    const highlightMenu = () => { 
+        const elem = document.querySelector('.highlight')
+        const homeMenu = document.querySelector('#home-page')
+        const aboutMenu = document.querySelector('#about-page')
+        const servicesMenu = document.querySelector('#services-page')
+        let scrollPos = window.scrollY
+
+        //adds 'highlight' class to my menu items
+        if (window.innerWidth > 960 && scrollPos < 600) {
+            
+        }
+    }
