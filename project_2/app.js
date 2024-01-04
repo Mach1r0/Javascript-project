3905c1e2db9a6d68298b43ccdb53b4022ff36fad
Mach1r0 <segundacontafd@gmail.com>
2024-01-04 00:55:16 -0300

    Integrate the API in html

--- project_2/app.js
+++ project_2/app.js
@@ -1,10 +1,20 @@
-    const apiKey = "5096d6e527abbe0a0ae35219fd5249a5";
-    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
-    
-    async function checkWeather(){
-        const response = await fetch(apiUrl + `&appid=${apiKey}`);
-        var data = await response.json();
+const apiKey = "5096d6e527abbe0a0ae35219fd5249a5";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-        console.log(data);
-    }
-    checkWeather();
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search input");
+
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city`&appid=${apiKey}`);
+  var data = await response.json();
+
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+}
+checkWeather(city);
