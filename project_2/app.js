f376fbf61342a36dc30b12cc2a60fdf970d87d60
Mach1r0 <segundacontafd@gmail.com>
2024-01-04 19:15:03 -0300

    fix the search

--- project_2/app.js
+++ project_2/app.js
@@ -3,8 +3,7 @@ const apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
-const searchBtn = document.querySelector(".search input");
-
+const searchBtn = document.querySelector(".search button");
 
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city`&appid=${apiKey}`);
@@ -17,4 +16,9 @@ async function checkWeather(city) {
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
   document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 }
+
 checkWeather(city);
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+});
