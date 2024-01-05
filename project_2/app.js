a9c9aa1dfb5060636c5a682d26fd56c0a84ee85e
Mach1r0 <segundacontafd@gmail.com>
2024-01-04 23:09:06 -0300

    finish the project 2

--- project_2/app.js
+++ project_2/app.js
@@ -4,6 +4,7 @@ const apiUrl =
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weater-icon");
 
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city +`&appid=${apiKey}`);
@@ -15,7 +16,19 @@ async function checkWeather(city) {
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
   document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
-}
+
+  if (data.weather[0].main == "Clouds") {
+    weatherIcon.src = "images/clouds.png";
+  }
+  else if (data.weather[0].main == "Rain"){
+    weatherIcon.src = "images/clouds.png";
+  }
+  else if (data.weather[0].main == "Drizzle"){
+    weatherIcon.src = "images/drizzle.png";
+  }
+  else if (data.weather[0].main == "Mist"){
+    weatherIcon.src = "images/mist.png";
+  }
 
 searchBtn.addEventListener("click", ()=>{
     checkWeather(searchBox.value);
